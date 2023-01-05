fead14d79262fb2ac972e6cd2fd0e63912dc4671
Jon Bell <github@lot23.com>
2023-01-05 05:10:28 +0000

    Putting variables in one place

--- src/devtools/client/debugger/src/components/SecondaryPanes/FrameTimeline.tsx
+++ src/devtools/client/debugger/src/components/SecondaryPanes/FrameTimeline.tsx
@@ -10,7 +10,6 @@ import React, { Component, Suspense, useContext } from "react";
 import ReactTooltip from "react-tooltip";
 
 import { locationsInclude } from "protocol/utils";
-import { getFrameStepsSuspense } from "replay-next/src/suspense/FrameStepsCache";
 import { ReplayClientContext } from "shared/client/ReplayClientContext";
 import { actions } from "ui/actions";
 import {
@@ -27,6 +26,8 @@ import { PartialLocation } from "../../actions/sources/select";
 import { PauseFrame, getExecutionPoint } from "../../reducers/pause";
 import { SymbolEntry, getSymbols } from "../../selectors";
 import { getSelectedFrameSuspense } from "../../selectors/pause";
+
+import { getFrameStepsSuspense } from "replay-next/src/suspense/FrameStepsCache";
 
 function getBoundingClientRect(element?: HTMLElement) {
   if (!element) {
